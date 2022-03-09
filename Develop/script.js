155b1fe4898be092d8ef25598f30c207681fef00
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-03-08 20:44:37 -0800

    set up drop down menu

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,93 @@
 // Assignment code here
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCaseChars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var specialChars = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
 
+function generatePassword() {
+
+}
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,6 +98,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+var lowercase = document.getElementById("lowercase").value;
+console.log(lowercase);
+var lowercaseCheck = document.querySelector("#lowercase");
+lowercaseCheck.addEventListener("click", test);
+function test() {
+  console.log(lowercase);
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Drop-down menu code here
+var setCrtr = document.querySelector("#set-criteria");
+var hidden = document.querySelector("#hidden");
+
+setCrtr.addEventListener("click", dropdown);
+
+function dropdown() {
+  hidden = document.getElementById("hidden").style.display = "inline";
+}
