26a65b4c91cac79bfb476be62326464c222e5940
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-03-09 00:51:34 -0800

    have finished the parsing for all the options, almost have the gateway to the randomizer function ready

--- Develop/script.js
+++ Develop/script.js
@@ -98,13 +98,78 @@ function writePassword() {
   passwordText.value = password;
 
 }
-var lowercase = document.getElementById("lowercase").value;
-console.log(lowercase);
+var count = 0;
+
+var lowercase = false;
 var lowercaseCheck = document.querySelector("#lowercase");
-lowercaseCheck.addEventListener("click", test);
-function test() {
-  console.log(lowercase);
+lowercaseCheck.addEventListener("click", lowerclick);
+function lowerclick() {
+  count++;
+  if (count % 2 !== 0) {
+    lowercase = true;
+  }
+  else {
+    lowercase = false;
+  }
 }
+
+var uppercase = false;
+var uppercaseCheck = document.querySelector("#uppercase");
+uppercaseCheck.addEventListener("click", upperclick);
+function upperclick() {
+  count++;
+  if (count % 2 !== 0) {
+    uppercase = true;
+  }
+  else {
+    uppercase = false;
+  }
+}
+
+var numbercase = false;
+var numberCheck = document.querySelector("#number");
+numberCheck.addEventListener("click", numberclick);
+function numberclick() {
+  count++;
+  if (count % 2 !== 0) {
+    numbercase = true;
+  }
+  else {
+    numbercase = false;
+  }
+}
+
+var specialcase = false;
+var specialCheck = document.querySelector("#number");
+specialCheck.addEventListener("click", specialclick);
+function specialclick() {
+  count++;
+  if (count % 2 !== 0) {
+    numbercase = true;
+  }
+  else {
+    numbercase = false;
+  }
+}
+
+var lengthcase = 0;
+var lengthCheck = document.getElementById("length");
+lengthCheck.addEventListener("keyup", function() {
+  lengthcase = lengthCheck.value;
+  console.log(lengthcase);
+});
+
+var buttonOn = document.getElementById("generate").disabled = true;
+function buttonVld() {
+  console.log("hi");
+  if (lengthcase > 7 && lengthcase < 129) {
+    if( specialcase == true || lowercase == true || uppercase == true || numbercase == true){
+      buttonOn.disabled = false;
+    }
+  }
+}
+ 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
